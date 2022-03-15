413b3ec5cd5eeec69b3bbd879a31e2c972d52d4b
dev-siv <sivasai10848@gvpce.ac.in>
2022-03-15 12:56:52 +0530

    Move Zeroes to end

---
+++ programming_skills/day6/move_zeroes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        for index, val in enumerate(nums):
+            if val != 0:
+                nums[count] = val
+                if index != count:
+                    nums[index] = 0
+                count += 1
+        # return nums
+
+
+if __name__ == "__main__":
+    n = [10, 20, 0, 0, 30, 40]
+    s = Solution()
+    print(s.moveZeroes(n))
+
+
