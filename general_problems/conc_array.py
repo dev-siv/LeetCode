41d50b74dfbee368aba812129de2d02e6b725bf1
dev-siv <sivasai10848@gvpce.ac.in>
2022-03-17 01:03:04 +0530

    Add two arrays

---
+++ general_problems/conc_array.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/concatenation-of-array/
+"""
+
+
+def getConcatenation(nums):
+    return nums+nums
+
+
+if __name__ == "__main__":
+    n = [1, 2, 3]
+    print(getConcatenation(n))
