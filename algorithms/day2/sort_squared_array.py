a2fa869dd7bf3fa23c6b115bc60e4562748e385f
dev-siv <sivasai10848@gvpce.ac.in>
2022-03-25 00:04:07 +0530

    modified

--- algorithms/day2/sort_squared_array.py
+++ algorithms/day2/sort_squared_array.py
@@ -1,11 +1,16 @@
 class Solution:
     def sortedSquares(self, nums):
-        res = []
-        for index, val in enumerate(nums):
-            res.insert(index, val*val)
-
-        print(res)
+        res = [None] * len(nums)
+        left, right = 0, len(nums) - 1
+        for index in range(len(nums)-1, -1, -1):
+            if abs(nums[left]) > abs(nums[right]):
+                res[index] = nums[left] ** 2
+                left += 1
+            else:
+                res[index] = nums[right] ** 2
+                right -= 1
+        return res
 
 
 s = Solution()
-s.sortedSquares([-3, 0, 1, 2])
+print(s.sortedSquares([-3, 0, 1, 2]))
