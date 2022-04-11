709901f884154f655a0a689a59187f7b34dbe070
dev-siv <sivasai10848@gvpce.ac.in>
2022-04-11 22:22:45 +0530

    Naive solution for longest substring

--- general_problems/longest_substring.py
+++ general_problems/longest_substring.py
@@ -5,7 +5,19 @@ def lengthOfLongestSubstring(s):
         return 1
     if len(s) == len(set(s)):
         return len(s)
+    max_length = 0
+    for i in range(len(s)):
+        longest = s[i]
+        # if max_length > len(s)-i:
+        for j in range(i+1, len(s)):
+            if s[j] in longest:
+                break
+            else:
+                longest += s[j]
+            if len(longest) > max_length:
+                max_length = len(longest)
+    return max_length
 
 
 if __name__ == "__main__":
-    print(lengthOfLongestSubstring("pwwkew"))
+    print(lengthOfLongestSubstring("bbbb"))
