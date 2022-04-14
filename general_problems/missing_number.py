7ba53861f45571421a3236495ca722212947890e
dev-siv <sivasai10848@gvpce.ac.in>
2022-04-14 22:04:59 +0530

    Missing number optimized solution

--- general_problems/missing_number.py
+++ general_problems/missing_number.py
@@ -1,11 +1,16 @@
 def missingNumber(nums):
-    for n in range(len(nums)+1):
+    for n in range(len(nums) + 1):
         try:
             nums.remove(n)
         except ValueError:
             return n
-    # return nums
+
+
+def missing_number(nums):
+    n = len(nums)
+    return ((n * (n + 1)) // 2) - sum(nums)
 
 
 if __name__ == "__main__":
-    print(missingNumber([3,0,1]))
+    print(missingNumber([3, 0, 1]))
+    print(missing_number([3, 0, 1]))
